d36673c4a265466ef18397925b27773dd27f2af7
Keith Novak <keith@novakeith.net>
2022-01-30 10:15:44 -0700

    Initial commit. Tested with a test token and it worked.

---
+++ Readme.md
@@ -0,0 +1,11 @@
+#Readme.md
+
+Use this tool to brute force JWT tokens.
+
+Variables that need to be set:
+-myToken: the JWT token.
+-myAlpha: the keyspace the program will iterate over
+-myLength: the max guess length
+-minLength: the minimum guess length
+
+v2 will take keyword args from a command line to make this nicer to use.
---
+++ jwtbrute.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#jwt-bruteforce.py
+#Brute force JWT files based on alphabet and key length
+#set myToken, myAlpha, myLength, and minLength according to what you need
+
+import jwt
+import sys
+from itertools import chain, product
+
+found = False
+attempts = 0
+myToken = ""
+myAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+" 
+myLength = 4 #max length of guess
+minLength = 4 #min length of guess
+
+def brute(charset, keylength, keymin):
+    return (''.join(candidate)
+        for candidate in chain.from_iterable(product(charset, repeat=i)
+        for i in range(keymin, keylength + 1)))
+
+for keyl in brute(myAlpha, myLength, minLength):
+    attempts += 1
+    try:
+        jwt.decode(myToken, keyl, algorithms=["HS256"])
+        sys.stdout.write("Key Found! %s\n" % (keyl))
+        sys.stdout.flush()
+        found = True
+    except jwt.exceptions.InvalidSignatureError:
+        pass
+
+    #optional, but I like seeing progress in my terminal
+    if attempts % 1000 == 0:
+        sys.stdout.write("Currently on attempt %s\n" % (attempts))
+        sys.stdout.write("Current Guess: %s\n" % (keyl))
+
+    if found == True:
+        sys.exit("Key found in %s attempts." % (attempts))
